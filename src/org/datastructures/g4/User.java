3d2d9e8522fec53c7e306349fc224d455b24c53f
Diego Zavala <diego.zavala@unmsm.edu.pe>
2022-06-19 19:04:49 -0500

    Coneccion a bd exitosa

---
+++ src/org/datastructures/g4/User.java
@@ -0,0 +1,9 @@
+package org.datastructures.g4;
+
+public class User {
+    public String name;
+    public String email;
+    public String phone;
+    public String address;
+    public String password;
+}
