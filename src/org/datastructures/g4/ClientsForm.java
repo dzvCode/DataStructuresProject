7b22f8347a405fe8fc378955506782c1050b4ccb
Diego Zavala <diegozavala.code@gmail.com>
2022-06-24 23:21:07 -0500

    Primer avance

---
+++ src/org/datastructures/g4/ClientsForm.java
@@ -0,0 +1,12 @@
+package org.datastructures.g4;
+
+import javax.swing.*;
+
+public class ClientsForm {
+    public JButton button1;
+    public JButton button2;
+    public JButton button3;
+    public JButton button4;
+    public JButton button5;
+
+}
